d9bd5f5baad7ca73ce16f777e8a2c2dd4f04d29c
Erik <erikdstock@users.noreply.github.com>
2022-04-02 10:22:25 -0400

    initial commit

---
+++ db/migrate/20220402135024_create_events.rb
@@ -0,0 +1,13 @@
+class CreateEvents < ActiveRecord::Migration[7.0]
+  def change
+    create_table :events do |t|
+      t.references :event_host, null: false, foreign_key: true
+      t.datetime :start_at
+      t.datetime :end_at
+      t.string :name
+      t.text :description
+
+      t.timestamps
+    end
+  end
+end
